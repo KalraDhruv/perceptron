9d0640098c974c7c687574079893908f4fa58daa
KalraDhruv <dhruvkalra@proton.me>
2024-07-22 17:13:56 +0000

    Added And and Or classifications

--- perceptron.m
+++ perceptron.m
@@ -1,23 +1,22 @@
 % Step 1 Initializing inputs 
-inputs = [-1,rand(1,5)];
-targets = [0,1,1,1,0];
+inputs = [1 1 0 0; 1 0 1 0];
+
+% The below targets are for OR Operation
+targets = [1 1 1 0];
+
+% The below targets are for AND Operation
+% targets = [1 0 0 0]
 learning_rate =0.1;
-epochs =10;
+epochs = 10;
 
 % Step 2 Initializing weights for the system
 [rows, columns] = size(inputs);
 
-% Initial value == 1 for the bias term 
-weights = [1,rand(rows,columns-1)];
+weights = rand(rows);
 disp('The weights are: ');
 disp(weights);
 
-% Step 3 Taking the weighted sum of input*weight
-weighted_sum = inputs * weights';
-disp('The weighted_sum is: ');
-disp(weighted_sum);
-
-% Step 5 Training the Perceptron
+% Step 4 Training the Perceptron
 for epoch = 1:epochs
     for i = 1:columns
         input = inputs(:,i);
@@ -31,11 +30,9 @@ for epoch = 1:epochs
     end
 end
 
-% Step 5 Classifying the result based on function
-disp('The classification label for the inputs is: ');
-disp(step_function(weighted_sum));
 
-% Step 4 Defining the stepFunction 
+% Step 3 Defining the stepFunction 
+
 function result = step_function(number)
     if number >= 0
         result = 1 ;
